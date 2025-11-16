79ad70d3eb668c9054f2a619f3d6f99927645bef
sircrazypaper <127138479+sircrazypaper@users.noreply.github.com>
2025-11-16 06:34:42 +0100

    update

--- xRM-calculator.js
+++ xRM-calculator.js
@@ -1,15 +1,6 @@
-let oneRM = 0;
-let twoRM = 0;
-let threeRM = 0;
-let fourRM = 0;
-let fiveRM = 0;
-let sixRM = 0;
-let sevenRM = 0;
-let eightRM = 0;
-let nineRM = 0;
-let tenRM = 0;
-let elevenRM = 0;
-let twelveRM = 0;
-
-let inputWeight = 0;
-let inputReps = 0;
+function calculator(){
+  let inputWeight = document.getElementById('weight').value;
+  let inputReps = document.getElementById('reps').value;
+  number = inputWeight * inputReps;
+  document.getElementById('display').innerHTML = number;
+}
