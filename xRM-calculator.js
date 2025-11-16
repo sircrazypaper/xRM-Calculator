8d9a6a89845ae5f50312aa75f32446abdfeb56a5
sircrazypaper <127138479+sircrazypaper@users.noreply.github.com>
2025-11-16 08:23:30 +0100

    bug fixes

--- xRM-calculator.js
+++ xRM-calculator.js
@@ -1,12 +1,11 @@
 let pctXRM = 100;
 let oneRM = 0;
-let multiplier = 1;
 
 function calculator(){
   event.preventDefault();
   //setup
-  let inputWeight = document.getElementById('weight').value;
-  let inputReps = document.getElementById('reps').value;
+  let inputWeight = parseFloat(document.getElementById('weight').value);
+  let inputReps = parseInt(document.getElementById('reps').value);
 
   //set 1RM
   if(inputReps == 1){
@@ -40,7 +39,7 @@ function calculator(){
   let twoRM = oneRM * 0.975;
   let threeRM = oneRM * 0.95;
   let fourRM = oneRM * 0.925;
-  let fiveRM = oneRM * 0.9
+  let fiveRM = oneRM * 0.9;
   let sixRM = oneRM * 0.85;
   let sevenRM = oneRM * 0.825;
   let eightRM = oneRM * 0.8;
@@ -50,16 +49,16 @@ function calculator(){
   let twelveRM = oneRM * 0.7;
 
   //display stats in html
-  document.getElementById("1rm").innerHTML = "1RM = " + oneRM;
-  document.getElementById("2rm").innerHTML = "2RM = " + twoRM;
-  document.getElementById("3rm").innerHTML = "3RM = " + threeRM;
-  document.getElementById("4rm").innerHTML = "4RM = " + fourRM;
-  document.getElementById("5rm").innerHTML = "5RM = " + fiveRM;
-  document.getElementById("6rm").innerHTML = "6RM = " + sixRM;
-  document.getElementById("7rm").innerHTML = "7RM = " + sevenRM;
-  document.getElementById("8rm").innerHTML = "8RM = " + eightRM;
-  document.getElementById("9rm").innerHTML = "9RM = " + nineRM;
-  document.getElementById("10rm").innerHTML = "10RM = " + tenRM;
-  document.getElementById("11rm").innerHTML = "11RM = " + elevenRM;
-  document.getElementById("12rm").innerHTML = "12RM = " + twelveRM;
+  document.getElementById("1rm").innerHTML = "1RM = " + oneRM.toFixed(2);
+  document.getElementById("2rm").innerHTML = "2RM = " + twoRM.toFixed(2);
+  document.getElementById("3rm").innerHTML = "3RM = " + threeRM.toFixed(2);
+  document.getElementById("4rm").innerHTML = "4RM = " + fourRM.toFixed(2);
+  document.getElementById("5rm").innerHTML = "5RM = " + fiveRM.toFixed(2);
+  document.getElementById("6rm").innerHTML = "6RM = " + sixRM.toFixed(2);
+  document.getElementById("7rm").innerHTML = "7RM = " + sevenRM.toFixed(2);
+  document.getElementById("8rm").innerHTML = "8RM = " + eightRM.toFixed(2);
+  document.getElementById("9rm").innerHTML = "9RM = " + nineRM.toFixed(2);
+  document.getElementById("10rm").innerHTML = "10RM = " + tenRM.toFixed(2);
+  document.getElementById("11rm").innerHTML = "11RM = " + elevenRM.toFixed(2);
+  document.getElementById("12rm").innerHTML = "12RM = " + twelveRM.toFixed(2);
 }
